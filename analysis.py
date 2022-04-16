8e6e68844947bc90607cd90ef967a3be4f0d9241
jonclindaniel <jclindaniel@uchicago.edu>
2022-04-16 17:50:08 -0500

    Add 'path' param to 'plot' function

--- analysis.py
+++ analysis.py
@@ -10,10 +10,12 @@ def process_data(data):
     df = pd.read_csv(data)
     return df
 
-def plot(data, save_fig=False):
+def plot(data, save_fig=False, path="./figure1.tiff"):
     '''
     Input: `data`: Pandas DataFrame with two columns of data (float64),
-           `save_fig`: bool indicating whether to save figure to file or not
+           `save_fig`: bool indicating whether to save figure to file (tiff)
+           `path`: string indicating path for saving figure if `save_fig` is
+                   True (defaults to "figure1.tiff" in working directory)
     Returns: Nothing (produces plot and optionally saves to file)
     '''
     sns.lmplot(x="column1",
@@ -22,12 +24,14 @@ def plot(data, save_fig=False):
     plt.title("Column 2 vs. Column 1")
 
     if save_fig:
-        plt.savefig("/figures/figure1.tiff",
+        plt.savefig(path,
                     dpi=300,
                     format="tiff",
                     bbox_inches="tight",
                     pil_kwargs={"compression": "tiff_lzw"})
 
 if __name__ == "__main__":
+    # When running script in Docker Container, save figure to /figures/
+    # directory
     df = process_data("data.csv")
-    plot(data=df, save_fig=True)
+    plot(data=df, save_fig=True, path="/figures/figure1.tiff")
